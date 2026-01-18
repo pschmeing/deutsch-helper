d7a39b16687b8d3c74b46fa0e462d9a5d1fd7367
pschmeing <83138772+pschmeing@users.noreply.github.com>
2026-01-18 14:32:45 +0100

    fix biuild issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/demo-salon-noir/",
+  base: "/demo-salon-elegance/",
   server: {
     host: "::",
     port: 8080,
